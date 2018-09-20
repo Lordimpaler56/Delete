9e6bb07898a22cce48e495d09b5e9a984317ff1f
LoganCuthbertson <37085695+Lordimpaler56@users.noreply.github.com>
2018-09-20 13:23:45 -0600

    update

--- Swapping Variables/index.js
+++ Swapping Variables/index.js
@@ -1,29 +1,9 @@
-console.log('Hello World');
-
 let firstName = "Logan";
-let middleName = "Cael";
 let lastName = "Cuthbertson";
-const age = 16;
-let height = "6 Feet";
-let eyeColour = "Light Blue";
-let hair = "Curly";
-let hairColour = "Brown";
-let favouriteFood = "Lasagna";
-let favouriteHobby = "Videogames";
-let favouriteBook = null;
-//debug with typeof
-let male = true;
+
+let firstName = "Cuthbertson";
+let lastName = "Logan";
 
 //print variables to console
 console.log(firstName);
-console.log(middleName);
 console.log(lastName);
-console.log(age);
-console.log(height);
-console.log(eyeColour);
-console.log(hair);
-console.log(hairColour);
-console.log(favouriteFood);
-console.log(favouriteHobby);
-console.log(favouriteBook);
-console.log("Am I male? " + male);
