4a5365bb2c80b08cf781f2809aef26d2aa44343c
LoganCuthbertson <37085695+Lordimpaler56@users.noreply.github.com>
2018-09-20 12:52:56 -0600

    update

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('Hello World');
+
+let firstName = "Logan";
+let middleName = "Cael";
+let lastName = "Cuthbertson";
+let age = "16";
+let height = "6 Feet";
+let eyeColour = "Light Blue";
+let hair = "Curly";
+let hairColour = "Brown";
+let favouriteFood = "Lasagna";
+let favouriteHobby = "Videogames";
+
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(age);
+console.log(height);
+console.log(eyeColour);
+console.log(hair);
+console.log(hairColour);
+console.log(favouriteFood);
+console.log(favouriteHobby);
