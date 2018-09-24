fda2ebd70fc5a97c48a758b9cf58f048ed942718
LoganCuthbertson <37085695+Lordimpaler56@users.noreply.github.com>
2018-09-24 12:53:08 -0600

    update

---
+++ Variables Arrays Start/index.js
@@ -0,0 +1,42 @@
+console.log('Hello World');
+
+let LoganCuthbertson : {=
+  firstName : "Logan",
+  middleName : "Cael",
+  lastName : "Cuthbertson",
+  age : 16,
+  height : "6 Feet",
+  eyeColour : "Light Blue",
+  hair : "Curly",
+  hairColour : "Brown",
+  favouriteFood : "Lasagna",
+  favouriteHobby : "Videogames",
+  favouriteBook : null,
+  male : true,
+}
+
+
+let LoganCuthbertson : {=
+  firstName : "Logan",
+  middleName : "Cael",
+  lastName : "Cuthbertson",
+  age : 16,
+  height : "6 Feet",
+  eyeColour : "Light Blue",
+  hair : "Curly",
+  hairColour : "Brown",
+  favouriteFood : "Lasagna",
+  favouriteHobby : "Videogames",
+  favouriteBook : null,
+  male : true,
+}
+
+//debug with typeOf
+
+//print variables to console
+
+//printing out entire object
+console.log(LoganCuthbertson);
+console.log(LoganCuthbertson.firstName);
+
+//console.log("Am I male? " + male);
