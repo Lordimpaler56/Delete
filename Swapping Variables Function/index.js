077ed9fef3f665a518577f87775392011a90c89a
LoganCuthbertson <37085695+Lordimpaler56@users.noreply.github.com>
2018-09-24 13:55:45 -0600

    update

---
+++ Swapping Variables Function/index.js
@@ -0,0 +1,11 @@
+// Your Function Goes Here
+let LoganCuthbertson = [];
+
+//firstName, middleName, lastName, height, age, eyeColour, hair, hairColour, favouriteFood, favouriteHobby, favouriteBook, male
+LoganCuthbertson = ["Logan", "Cael", "Cuthbertson", 16, "6 feet", "Light Blue", "Curly", "Brown", "Lasagna", "Videogames", null, true];
+
+// Your Code Goes Here
+
+
+//print first names only
+console.log("Last Name of LoganCuthbertson is: " + )
