4007f877fc03a9648a57e60abaea36a5f828f429
LoganCuthbertson <37085695+Lordimpaler56@users.noreply.github.com>
2018-09-27 13:26:29 -0600

    update

--- Operators/index.js
+++ Operators/index.js
@@ -1,16 +1,22 @@
-// Hello World Function
-function greet (firstName, lastName){ //parameters
- console.log('Hello ' + firstName + ' World '  + lastName);
+function simpleOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB;
+  let mutliply = numberA * numberB;
+  let divide = numberA / numberB;
+  console.log(adding);
+  console.log(subtract);
+  console.log(mutliply);
+  console.log(divide);
 }
 
-function square (number) {
-  return number*number;
+function skipCounting (skipCount, times, startingNumber){
+  let number = startingNumber - skipCount;
+  for (let i=1; i<=times; i++){
+    number += skipCount;
+    console.log(number);
+  }
 }
-
-
-//firstName, middleName, lastName, height, age, eyeColour, hair, hairColour, favouriteFood, favouriteHobby, favouriteBook, male
-let LoganCuthbertson = ["Logan", "Cael", "Cuthbertson", 16, "6 feet", "Light Blue", "Curly", "Brown", "Lasagna", "Videogames", null, true];
-
-//Calling Functions, with arguements
-greet (LoganCuthbertson[0], LoganCuthbertson[2]) //Arguement
-greet (square(LoganCuthbertson[2]));
+let number = [4, 7, 13, 99];
+simpleOperators (number[0], number[1]);
+simpleOperators (number[2], number[3]);
+skipCounting(3, 6, 113);
