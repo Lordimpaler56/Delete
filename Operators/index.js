6c385858d8e1465095db771a69f893f09644ec94
LoganCuthbertson <37085695+Lordimpaler56@users.noreply.github.com>
2018-10-04 12:22:43 -0600

    update

--- Operators/index.js
+++ Operators/index.js
@@ -8,28 +8,56 @@ function simpleOperators (numberA, numberB){
   console.log(mutliply);
   console.log(divide);
 }
-
+//Skipcount by ___ for ___ times starting at ___
 function skipCounting (skipCount, times, startingNumber){
   let number = startingNumber - skipCount;
   for (let i=1; i<=times; i++){
-  console.log(number);
-  number += 5;
-
+    number += skipCount;
+    console.log(number);
   }
 }
 
+//simpleComparison using WHILE and Skip Counting Algorithm
 function howManyTimes (numberStart, numberEnd, skipCount) {
-while (numberStart < numberEnd) {
-console.log(numberStart);
-numberStart += skipCount;
- }
+  while (numberStart < numberEnd) {
+    console.log(numberStart += skipCount); // Used to be 2 lines ... console.log(numberStart); numberStart += skipCount;
+    // What is happening with the single line compared to the double line of code?
+  }
 }
-//Breaks while loop into zero loop
-console.log(1 == 1);
-console.log('1' == 1);
-console.log('1' === 1);
 
-let number = [4, 7, 13, 99, 0, 100, 5];
-//simpleOperators (number[2], number[3]);
-//skipCounting(3, 6, 113);
-howManyTimes (0, 100, 5)
+function LooseEquality (firstOperand, secondOperant) { //Changes the second operand to the typeof the first operand
+  console.log(firstOperand == secondOperant);
+}
+
+function StrictEquality (firstOperand, secondOperant) {
+  console.log(firstOperand === secondOperant);
+}
+
+function notEqual(valueArray, ){
+  if (valueArray !=5){
+    console.log(number[i], "Is index number: " i, "with a total of :", i); i=number.length
+    return i;
+  } else {console.log("5 is at the index", i); i=number.length
+}
+
+//Not Equality Starter Code
+//Outputing individual values of an array, we want to start stop at the value 5, where ever it is in the array
+
+
+let number = [4, 7, 13, 99, 0, 100, 5, 1, '1']; //able to add any typeof to an array, index is automatic
+for (let i=0; i<number.length; i++) { //we do not know when to stop in the array
+notEqual (if i == )
+if (number[i] !=5) { //This is when we do stop, so we accept every other value (common logic for "not")
+console.log(number[i], "Is index number: ", i, "with a total array index of" number.length;}
+else {console.log("5 is at the index", i); i=number.length} //once we stop, we want to jump out of the loop
+}
+}
+//Introduces Break and Continue Code
+//See: https://github.com/QEHS-Websites/JavaScript-Sandbox/tree/master/Control%20Flow#break-and-continue-key-words-and-loop-concepts
+
+
+//Using falsy preferences
+let userColor = 'red'; //User not defined yet
+let defaultColor = 'blue'; //default colour chosen by developer
+let currentColor = userColor || defaultColor
+console.log(currentColor); //returns 'blue'
